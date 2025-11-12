c7df8a54e04fe3310dc154557a505333679cef6d
bharathiraja-15 <bharathimtech11@gmail.com>
2025-11-12 15:56:12 +0530

    Add AWS variables for Terraform configuration

---
+++ variables.tf
@@ -0,0 +1,37 @@
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  default     = ""
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  default     = ""
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-north-1"
+}
+
+
+variable "ami" {
+   type        = string
+   description = "Amazon Linux AMI ID"
+   default     = "ami-05535ba2f47d4b7a2"
+}
+
+variable "instance_type" {
+   type        = string
+   description = "Instance type"
+   default     = "t3.micro"
+}
+
+variable "name_tag" {
+   type        = string
+   description = "Name of the EC2 instance"
+   default     = "Terraform-plugin"
+}
+
