03edb9e9c3e471caf64f774473ec915554e8551c
SyedAbdurRazzaq <syedmusharraf.max@gmail.com>
2025-11-12 17:50:16 +0530

    Update AWS instance configuration in main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "public_instance" {
-  ami                    = "ami-0c7d68785ec07306c"
-  instance_type          = "t3.micro"
+  ami                    = "ami-0bdd88bd06d16ba03"
+  instance_type          = "c7i-flex.large"
 
-  subnet_id              = "subnet-0d91e8fd1fa2524c9"
-  vpc_security_group_ids = ["sg-0a9dbf56ebd34521a"]
+  subnet_id              = "subnet-0398cec97156f1f60"
+  vpc_security_group_ids = ["sg-0f42aca177a110e2e"]
 
   tags = {
     Name = "Terraform-plugin"
